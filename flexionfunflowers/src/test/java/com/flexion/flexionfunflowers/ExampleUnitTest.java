3d66af7d5cafbabc57e92b55ee2a51df18744671
Peter Ruprecht <peter.ruprecht@p92rdi.hu>
2017-07-19 10:44:30 +0200

    Added some test cases

--- flexionfunflowers/src/test/java/com/flexion/flexionfunflowers/ExampleUnitTest.java
+++ flexionfunflowers/src/test/java/com/flexion/flexionfunflowers/ExampleUnitTest.java
@@ -1,7 +1,12 @@
 package com.flexion.flexionfunflowers;
 
+import com.flexion.flexionfunflowers.helpers.FlexionConsts;
+import com.flexion.flexionfunflowers.models.FlexionPurchase;
+import com.flexion.flexionfunflowers.utils.FlexionIntegration;
 import com.flexionmobile.codingchallenge.integration.IntegrationTestRunner;
+import com.flexionmobile.codingchallenge.integration.Purchase;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,8 +17,32 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest extends IntegrationTestRunner {
+
+    private FlexionIntegration flexionIntegration;
+
+    @Before
+    public void initialize() {
+        flexionIntegration = new FlexionIntegration(FlexionConsts.DEVELOPERID);
+    }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void test_buy() throws Exception {
+        Purchase purchase = flexionIntegration.buy("item1");
+        assertEquals("item1", purchase.getItemId());
+    }
+
+    @Test
+    public void test_getPurchase() throws Exception {
+        int beforeCount = flexionIntegration.getPurchases().size();
+        flexionIntegration.buy("item2");
+        assertEquals(beforeCount++, flexionIntegration.getPurchases().size());
+    }
+
+    @Test
+    public void test_consume() throws Exception {
+        int beforeCount = flexionIntegration.getPurchases().size();
+        Purchase purchase = flexionIntegration.buy("item3");
+        flexionIntegration.consume(purchase);
+        assertEquals(beforeCount, flexionIntegration.getPurchases().size());
     }
 }
